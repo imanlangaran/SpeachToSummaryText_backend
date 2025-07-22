7a3c7f767375238370df6656c12a6676885a5118
Iman Langaran <imanlangaran@gmail.com>
2025-07-22 17:17:20 +0330

    phase 2 : implement auth

---
+++ app/auth/auth_utils.py
@@ -0,0 +1,14 @@
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from app.models.user import User
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(User).filter(User.email == email).first()
