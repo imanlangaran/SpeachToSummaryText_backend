3ee4917832c5e8adb07879fa088f04ec2b9367e9
Iman Langaran <imanlangaran@gmail.com>
2025-07-22 17:22:28 +0330

    phase 2 : implement auth

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+from fastapi import Depends
+from app.auth.dependencies import get_current_user
 from fastapi import FastAPI
 from app.db.database import Base, engine
 from app.models import user
+from app.auth import auth_router
 
 app = FastAPI()
 
-# Auto-create tables (only for dev!)
 Base.metadata.create_all(bind=engine)
+
+app.include_router(auth_router.router)
 
 @app.get("/")
 def root():
-    return {"msg": "Voice summarizer backend (SQLite) is running"}
+    return {"msg": "Voice summarizer backend with auth"}
+
+@app.get("/me")
+def read_me(user_email: str = Depends(get_current_user)):
+    return {"email": user_email}
